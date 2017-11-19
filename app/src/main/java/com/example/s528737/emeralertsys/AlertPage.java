f86d1a2cd079e274bdfeb20329d10560d433d0bf
raghavender jarupati <raghavender.jarupati@mail.nwmissouri.edu>
2017-11-18 21:09:43 -0600

    Added intents to alertPage activity

---
+++ app/src/main/java/com/example/s528737/emeralertsys/AlertPage.java
@@ -0,0 +1,61 @@
+package com.example.s528737.emeralertsys;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class AlertPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_page2);
+    }
+
+
+    public void savePassword(View v){
+
+        Login emg = signInResult.getSignInAccount();
+        Credential credential = new Credential.Builder(emg.getEmail())
+                .setAccountType(IdentityProviders.GOOGLE)
+                .setName(emg.getDisplayName())
+                .build();
+        Toast.makeText(getApplicationContext(),"password saved",Toast.LENGTH_LONG).show();
+    }
+
+
+    public void contactsPage(View v){
+
+        Intent contacts= new Intent(this,AppContacts.class);
+        startActivity(contacts);
+
+    }
+
+    public void toFeedback1(View v){
+        Intent emer1= new Intent(this,Feedback.class);
+        startActivity(emer1);
+    }
+    public void toFeedback2(View v){
+        Intent emer2= new Intent(this,Feedback.class);
+        startActivity(emer2);
+    }
+    public void toFeedback3(View v){
+        Intent emer3 = new Intent(this,Feedback.class);
+        startActivity(emer3);
+    }
+
+    public void toMainFeedback(View v)
+    {
+        Intent mainFeed = new Intent(this,Feedback.class);
+        startActivity(mainFeed);
+    }
+
+
+    public void menuBar(View v){
+        Intent menubar = new Intent(this,Feedback.class);
+        startActivity(menubar);
+    }
+
+}
